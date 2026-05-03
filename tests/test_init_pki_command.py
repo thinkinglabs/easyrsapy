c8d5b3fd45a7b5e20c46632e958fe0b2e8ad708f
Thierry de Pauw <thierry@thinkinglabs.io>
2026-05-03 18:37:11 +0200

    Fix failing build
    test_init_pki_command fails because the build uses easyrsa 3.1.7
    vs 3.2.6. 3.1.7 outputs two additional lines in the notice.
    Therefore relax the assert to check whether the expected is part
    of the returned actual.

--- tests/test_init_pki_command.py
+++ tests/test_init_pki_command.py
@@ -11,13 +11,13 @@ def test_init_pki():
                   'init-pki' complete; you may now create a CA or requests.
                   
                   Your newly created PKI dir is:
-                  * /tmp/test_pki
-                  
+                  * /tmp/pki
+
                   """  # noqa: W293
-    pki_dir = "/tmp/test_pki"
+    pki_dir = "/tmp/pki"
     command = InitPkiCommand(easy_rsa_path="easyrsa")
     request = InitPkiRequest(pki_dir=pki_dir)
     response = command.execute(request)
     actual = response.notice
-    assert textwrap.dedent(expected) == actual
+    assert textwrap.dedent(expected) in actual
     assert pki_dir == response.pki_dir
